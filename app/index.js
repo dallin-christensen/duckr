0cfe9ab1a2714b60abd7d28a6bd4a705e5323349
Dallin <christensen.dallin@gmail.com>
2018-02-08 19:59:24 -0800

    React startup

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class Main extends React.Component{
+  render(){
+    return (
+      <p>Hello World!</p>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('app')
+)
