cee82bbc865a5a20ff767cdfaa1d591163391265
Dallin <christensen.dallin@gmail.com>
2018-02-17 14:50:07 -0800

    Home Route
    Finished Home Route, implementation of CSS Modules

--- app/containers/Main/MainContainer.js
+++ app/containers/Main/MainContainer.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Navigation } from 'components'
+import { container, innerContainer } from './styles.css'
 
 class MainContainer extends React.Component {
   render () {
     return (
-      <div>
-        {this.props.children}
+      <div className={container}>
+        <Navigation isAuthed={true}/>
+        <div className={innerContainer}>
+          {this.props.children}
+        </div>
       </div>
     )
   }
