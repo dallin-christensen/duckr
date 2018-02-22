c43e9f37f8f7c08e874f00d4efa32a4ea06e63cf
Dallin <christensen.dallin@gmail.com>
2018-02-21 20:36:24 -0800

    Implementation of Redux-thunk for AuthContainer

--- app/containers/Authenticate/AuthenticateContainer.js
+++ app/containers/Authenticate/AuthenticateContainer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
 import { Authenticate } from 'components'
-import auth from 'helpers/auth'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as userActionCreators from 'redux/modules/users'
@@ -11,21 +10,12 @@ class AuthenticateContainer extends React.Component{
   propTypes: {
     isFetching: PropTypes.bool.isRequired,
     error: PropTypes.string.isRequired,
-    authUser: PropTypes.func.isRequired,
-    fetchingUser: PropTypes.func.isRequired,
-    fetchingUserSuccess: PropTypes.func.isRequired,
-    fetchingUserFailure: PropTypes.func.isRequired,
+    fetchAndHandleAuthedUser: PropTypes.func.isRequired,
   }
   handleAuth = () => {
-    this.props.fetchingUser()
-    auth().then((user) => {
-      this.props.fetchingUserSuccess(user.uid, user, Date.now())
-      this.props.authUser(user.uid)
-    })
-    .catch((error) => this.props.fetchUserFailure(error))
+    this.props.fetchAndHandleAuthedUser()
   }
   render () {
-    console.log('isFecthing',this.props.isFetching)
     return (
       <Authenticate
         isFetching={this.props.isFetching}
@@ -36,6 +26,7 @@ class AuthenticateContainer extends React.Component{
 }
 
 function mapStateToProps (state) {
+  console.log(state)
   return {
     isFetching : state.isFetching,
     error : state.error,
