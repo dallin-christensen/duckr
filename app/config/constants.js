9b25a1f0d06b2878b3008261542ece41ad86f92b
Dallin <christensen.dallin@gmail.com>
2018-02-26 21:29:32 -0800

    production prepping

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase'
 
 
+// const config = {
+//   apiKey: "AIzaSyCrFAyCCJeevpFhpO52cKjazDTC91Lfhtk",
+//   authDomain: "dallins-test-project.firebaseapp.com",
+//   databaseURL: "https://dallins-test-project.firebaseio.com",
+//   projectId: "dallins-test-project",
+//   storageBucket: "dallins-test-project.appspot.com",
+//   messagingSenderId: "435979016045"
+// };
+// firebase.initializeApp(config);
+
 const config = {
-  apiKey: "AIzaSyCrFAyCCJeevpFhpO52cKjazDTC91Lfhtk",
-  authDomain: "dallins-test-project.firebaseapp.com",
-  databaseURL: "https://dallins-test-project.firebaseio.com",
-  projectId: "dallins-test-project",
-  storageBucket: "dallins-test-project.appspot.com",
-  messagingSenderId: "435979016045"
+  apiKey: "AIzaSyAygs-SlkgfJvJ_Tdux7-hBpZ8Z30Bj16Y",
+  authDomain: "duckr-89aa1.firebaseapp.com",
+  databaseURL: "https://duckr-89aa1.firebaseio.com",
+  projectId: "duckr-89aa1",
+  storageBucket: "duckr-89aa1.appspot.com",
+  messagingSenderId: "22245849391"
 };
 firebase.initializeApp(config);
 
